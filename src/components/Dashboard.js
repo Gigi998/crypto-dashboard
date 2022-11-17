103a3673268ee73a95958efc71534640740d0774
Luigi <luigi.drnasin12@gmail.com>
2022-11-17 10:58:04 +0100

    Search bar styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,13 +5,29 @@ import styled from "styled-components";
 const Dashboard = () => {
   return (
     <Wrapper>
-      <Navbar />
-      <MainDashboard />
-      <SideDashboard />
+      <div className="content">
+        <Navbar />
+        <div className="dashboard">
+          <MainDashboard />
+          <SideDashboard />
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  background-color: #181a25;
+  border-top-right-radius: 25px;
+  border-bottom-right-radius: 25px;
+  height: 100%;
+  .content {
+    max-height: inherit;
+    height: 85vh;
+    margin: 1rem 1rem 0 1rem;
+    display: grid;
+    grid-template-rows: 1fr 9fr;
+  }
+`;
 
 export default Dashboard;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,68 @@
 import React from "react";
+import styled from "styled-components";
+import { BsHeart, BsBell, BsSearch } from "react-icons/bs";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Wrapper>
+      <form className="search-field" onSubmit={handleSubmit}>
+        <button className="search-icon">
+          <BsSearch />
+        </button>
+        <input type="text" placeholder="Search market" className="input" />
+      </form>
+      <div className="icons">
+        <div className="icon-container">
+          <BsHeart />
+        </div>
+        <div className="icon-container">
+          <BsBell />
+        </div>
+        <div className="icon-container">
+          <p>Welcome user</p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .icons {
+    display: flex;
+    align-items: center;
+  }
+  .search-field {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    background: linear-gradient(180deg, #171822 0%, #171822 100%);
+    border: 1px solid #303241;
+    border-radius: 12px;
+    width: 25vw;
+    padding: 6px;
+    max-width: 300px;
+  }
+  .input {
+    color: #a6a9b3;
+    border: none;
+    border-color: transparent;
+    background-color: transparent;
+    width: 100%;
+  }
+  .input:focus {
+    border-color: 3px solid blue;
+    border: none;
+  }
+  .search-icon {
+    color: #a6a9b3;
+    background-color: transparent;
+    border: none;
+  }
+`;
 export default Navbar;
